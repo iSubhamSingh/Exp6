3902928519f5ea600de6c9e39a09d5494d2d6808
Subham <subhamsingh5638@gmail.com>
2023-06-05 18:59:51 +0530

    initial commit

---
+++ src/app/mock-company.ts
@@ -0,0 +1,15 @@
+import { Company } from "./info";
+
+export const COMPANY: Company[] = [
+    {id:0,name:'PhonePe', category:'Elite', role:'Software Developer', location:'Pune', 
+    ctc:'33,00,000 LPA', date:'10am - 18 july 2023, ', process:'Coding Test->Technical Round->Hr Round', image:'../assets/phonepe.jpeg'},
+
+    {id:1,name:'JPMC', category:'Elite', role:'Software Engineer', location:'Mumbai', 
+    ctc:'12,00,000 LPA', date:'12am - 21 july 2023', process:'Aptitude Test->Technical Round->Technical Round', image:'../assets/jpmc.jpeg'},
+
+    {id:2,name:'Barclays', category:'Super Dream', role:'Software Programmer', location:'Bengaluru', 
+    ctc:'10,00,000 LPA', date:'10am - 23 july 2023', process:'Assignment->Technical Round->Demonstration', image:'../assets/barclays.jpeg'},
+
+    {id:3,name:'Nomura', category:'Dream', role:'Frontend Engineert', location:'Mumbai', 
+    ctc:'8,00,000 LPA', date:'10am - 25 july 2023', process:'Aptitude->Technical Round->HR Round', image:'../assets/nomura.jpeg'}
+];
